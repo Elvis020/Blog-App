292c5457aa605743ddb7602acfb9345d6adb9cac
Elvis020 <elvis.amoako@stu.ucc.edu.gh>
2020-10-10 23:26:20 +0000

    Added multer for upload functionality

--- Backend/app.js
+++ Backend/app.js
@@ -2,23 +2,57 @@ const express = require("express");
 const app = express();
 const Post = require("./api/models/post")
 const postData = new Post();
+const bodyParser = require("body-parser");
+const multer = require("multer");
+
+
+// Adding upload functionality to the app using multer
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${file.fieldname}_${Date.now()}${getExt(file.mimetype)}`);
+  },
+});
+
+const getExt = (mimeType) => {
+    switch (mimeType) {
+      case "image/png":
+        return ".png";
+      case "image/jpeg":
+        return ".jpg";
+    }
+}
+
+//  Uploading to storage
+let upload = multer({ storage: storage });
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     next();
 })
 
+app.use(bodyParser.json());
 
 // Making the Images available to the frontend
 app.use("/images", express.static('images'));
 
 app.get("/api", (req, res) => {
-
-    // const test = {
-    //     testing: "testing"
-    // }
-    // postData.addBlogPost(test)
     res.status(200).send(postData.readData())
+})
+
+app.post("/api/posts", upload.single("post_image"), (req,res) => {
+    const newPosts = {
+        "id": `${Date.now()}`,
+        "title": req.body.title,
+        "content": req.body.content,
+        "post_image":req.file.path,
+        "added_date": `${Date.now()}`
+    }
+    postData.addBlogPost(newPosts)
+    res.status(201);
+    res.send("ok");
 })
 
 
